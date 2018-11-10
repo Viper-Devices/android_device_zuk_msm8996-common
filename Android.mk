ce6862e14bd6361ace394613760a99fcc216da5b
cyproto <kyashkarande@gmail.com>
2018-11-10 09:42:04 +0000

    Adapt Utouch for Viper and other minor changes.

--- Android.mk
+++ Android.mk
@@ -1,3 +1,4 @@
+
 #
 # Copyright (C) 2013-2016, The CyanogenMod Project
 # Copyright (C) 2017, The LineageOS Project
--- msm8996.mk
+++ msm8996.mk
@@ -30,8 +30,8 @@ $(call inherit-product, $(LOCAL_PATH)/hidl.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-cr
-    
+    $(LOCAL_PATH)/overlay-Viper
+
 PRODUCT_ENFORCE_RRO_TARGETS := \
 framework-res
 
